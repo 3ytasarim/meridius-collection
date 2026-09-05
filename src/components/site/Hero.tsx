31ef77ce2e9aaa924c787fdff6fa82baa240715a
Rauf Onur Çullu <3ytasarim@gmail.com>
2026-09-05 21:26:46 +0000

    Changes

--- src/components/site/Hero.tsx
+++ src/components/site/Hero.tsx
@@ -12,7 +12,7 @@ const OrbitalSphere = React.lazy(() =>
   })),
 );
 
-const btnGradient = ["#FFFFFF", "#F4EFFF", "#A77AF4", "#FFFFFF"];
+
 
 export function Hero() {
   const navigate = useNavigate();
